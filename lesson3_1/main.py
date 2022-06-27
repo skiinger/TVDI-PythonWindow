73d99d1364fb75bec44219d0cabb50894c92f86f
tvdi_pc <skiinger0819@gmail.com>
2022-06-27 13:19:42 +0800

    Initial commit

---
+++ lesson3_1/main.py
@@ -0,0 +1,14 @@
+import  report as re #改名
+from report import PI,get_descruotuin,Student
+import source.weather as sw
+import source.daily as sd
+
+if __name__ == "__main__":
+    sw.callMyName()
+    sd.callMyName()
+
+    # print(get_descruotuin())
+    # print(PI)
+    # stu1 = Student()
+    # print(type(stu1))
+
